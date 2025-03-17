5a9d38561c27b82742f4481d9bf894196bae5b2a
OHTcb <o.haim3@st.tcb.ac.il>
2025-03-17 16:27:05 +0200

    downgraded next auth to v4

--- auth.ts
+++ auth.ts
@@ -1,58 +1,58 @@
-import NextAuth from 'next-auth';
-import type { NextAuthConfig } from 'next-auth';
-import Credentials from 'next-auth/providers/credentials';
-import { PrismaAdapter } from '@next-auth/prisma-adapter';
-import { db } from '@/db/connect';
-import { compareFunction } from '@/utils/hashFunction';
+  import NextAuth from 'next-auth';
+  import type { NextAuthOptions } from 'next-auth';
+  import Credentials from 'next-auth/providers/credentials';
+  import { PrismaAdapter } from '@next-auth/prisma-adapter';
+  import { db } from '@/db/connect';
+  import { compareFunction } from '@/utils/hashFunction';
 
-export const authOptions: NextAuthConfig = {
-  adapter: PrismaAdapter(db),
-  session: {
-    strategy: 'jwt',
-  },
-  pages: {
-    signIn: '/login',
-  },
-  providers: [
-    Credentials({
-      name: 'Credentials',
-      credentials: {
-        email: {
-          label: 'Email',
-          type: 'email',
-          placeholder: 'example@gmail.com',
+  export const authOptions: NextAuthOptions = {
+    adapter: PrismaAdapter(db),
+    session: {
+      strategy: 'jwt',
+    },
+    pages: {
+      signIn: '/login',
+    },
+    providers: [
+      Credentials({
+        name: 'Credentials',
+        credentials: {
+          email: {
+            label: 'Email',
+            type: 'email',
+            placeholder: 'example@gmail.com',
+          },
+          password: { label: 'Password', type: 'password' },
         },
-        password: { label: 'Password', type: 'password' },
-      },
-      async authorize(credentials) {
-        const email = credentials?.email as string;
-        const password = credentials?.password as string;
-      
-        if (!email || !password) {
-          return null;
-        }
-      
-        const existingUserByEmail = await db.user.findUnique({
-          where: { email },
-        });
-      
-        if (!existingUserByEmail) {
-          return null;
-        }
-      
-        const passwordMatches = await compareFunction(password, existingUserByEmail.password);
-      
-        if (!passwordMatches) {
-          return null;
-        }
-      
-        return { 
-          id: `${existingUserByEmail.id}`, 
-          email: existingUserByEmail.email 
-        };
-      },
-    }),
-  ],
-};
+        async authorize(credentials) {
+          const email = credentials?.email as string;
+          const password = credentials?.password as string;
+        
+          if (!email || !password) {
+            return null;
+          }
+        
+          const existingUserByEmail = await db.user.findUnique({
+            where: { email },
+          });
+        
+          if (!existingUserByEmail) {
+            return null;
+          }
+        
+          const passwordMatches = await compareFunction(password, existingUserByEmail.password);
+        
+          if (!passwordMatches) {
+            return null;
+          }
+        
+          return { 
+            id: `${existingUserByEmail.id}`, 
+            email: existingUserByEmail.email 
+          };
+        },
+      }),
+    ],
+  };
 
-export default NextAuth(authOptions);
+  export default NextAuth(authOptions);
