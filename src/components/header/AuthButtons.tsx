712745930561d6f2cbdc3888f8442652248345cd
OHLunar <osher5209@gmail.com>
2025-03-20 21:30:43 +0200

    Fixed the login/signup link hopefully this time

--- src/components/header/AuthButtons.tsx
+++ src/components/header/AuthButtons.tsx
@@ -7,13 +7,13 @@ const AuthButtons = ({ session }: { session: any }) => {
   ) : (
     <>
       <Link
-        href="./login"
+        href="/login"
         className="rounded-sm px-4 py-2 text-black shadow-sm transition hover:text-gray-500"
       >
         Log in
       </Link>
       <Link
-        href="./signup"
+        href="/signup"
         className="rounded-sm px-4 py-2 text-white bg-black shadow-sm transition hover:bg-gray-700"
       >
         Sign up
--- src/components/header/NavLinks.tsx
+++ src/components/header/NavLinks.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 const NavLinks = () => {
   return (
     <>
-      <Link href="./admin" className="text-black hover:underline">
+      <Link href="/admin" className="text-black hover:underline">
         Admin
       </Link>
       <Link href="#" className="text-black hover:underline">
